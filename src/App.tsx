87c19399e5e29aa00fec03bf0bc87d2e8b683b07
seongwoo <he1236@ajou.ac.kr>
2021-12-10 14:06:24 +0900

    ES Lint & Prettier 적용완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
 function App() {
-  return <div className="App">asdasd</div>;
+  return <div className="App">Is this work?</div>;
 }
 
 export default App;
