ab79c390299826acafb4547bc10be22a25c8a5d8
seongwoo <he1236@ajou.ac.kr>
2021-12-12 02:37:26 +0900

    index.tsx 실험

--- app/renderer/src/index.tsx
+++ app/renderer/src/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import MainScreen from './screens/MainScreen';
+import SubScreen from './screens/SubScreen';
 
 export default function App() {
-  return <MainScreen />;
+  return (
+    <div>
+      <MainScreen />
+      <SubScreen />
+    </div>
+  );
 }
---
+++ app/renderer/src/screens/SubScreen.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function SubScreen() {
+  return (
+    <div className="ui container">
+      <button className={'ui button'}>Follow me!</button>
+    </div>
+  );
+}
