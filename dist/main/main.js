29684570d07bba0e04404ba87725552d6cfb9568
leejaeseung <wasd222@naver.com>
2021-12-19 17:23:55 +0900

    패키지 업데이트

--- dist/main/main.js
+++ dist/main/main.js
@@ -867,8 +867,8 @@ eval("\n\n__webpack_require__(/*! source-map-support/source-map-support.js */ \"
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\JSW\Desktop\GIT\do-it-client\node_modules\electron-webpack\out\electron-main-hmr\main-hmr */"./node_modules/electron-webpack/out/electron-main-hmr/main-hmr.js");
-module.exports = __webpack_require__(/*! C:\Users\JSW\Desktop\GIT\do-it-client\app\main\index.ts */"./app/main/index.ts");
+__webpack_require__(/*! C:\Users\82107\Desktop\doit\do-it-client\node_modules\electron-webpack\out\electron-main-hmr\main-hmr */"./node_modules/electron-webpack/out/electron-main-hmr/main-hmr.js");
+module.exports = __webpack_require__(/*! C:\Users\82107\Desktop\doit\do-it-client\app\main\index.ts */"./app/main/index.ts");
 
 
 /***/ }),
