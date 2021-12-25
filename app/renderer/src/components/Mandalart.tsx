7e7c3e82a8bffd0b5cd33ff4e9c94b75bdeac1b9
leejaeseung <wasd222@naver.com>
2021-12-25 16:44:55 +0900

    만다라트 파일 생성

---
+++ app/renderer/src/components/Mandalart.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Box } from '@mui/material'
+
+export function Mandalart() {
+  return (
+    <Box>
+      <Box>asd</Box>
+      asd
+    </Box>
+  )
+}
--- app/renderer/src/index.tsx
+++ app/renderer/src/index.tsx
@@ -1,34 +1,11 @@
-import { AppBar, IconButton, Toolbar, Box } from '@mui/material'
+import { Box } from '@mui/material'
 import React, { useState } from 'react'
-import MenuIcon from '@mui/icons-material/Menu'
-import LeftGNB from './components/LeftGNB'
+import { Mandalart } from './components/Mandalart'
 
 export default function App() {
-  const [leftGnbOpen, setLeftGnbOpen] = useState(false)
-
-  const onOpenLeftGnb = () => {
-    setLeftGnbOpen(true)
-  }
-
-  const onCloseLeftGnb = () => {
-    setLeftGnbOpen(false)
-  }
-
   return (
     <Box>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            onClick={onOpenLeftGnb}
-          >
-            <MenuIcon />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-      <LeftGNB open={leftGnbOpen} onClose={onCloseLeftGnb} />
+      <Mandalart />
     </Box>
   )
 }
