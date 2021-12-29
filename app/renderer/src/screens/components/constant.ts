e4be5d8b7016d3519d8b2625676cc648a7d10f7f
seongwoo <he1236@ajou.ac.kr>
2021-12-29 15:35:40 +0900

    PlanTag의 타입 설정 및 PlanTag default값 설정 완료

---
+++ app/renderer/src/screens/components/constant.ts
@@ -0,0 +1,16 @@
+import { PlanTagType } from './type'
+
+export const defaultPlanTags: PlanTagType[] = [
+  {
+    id: 1,
+    planname: 'ICT 전략 및 기획 복습',
+  },
+  {
+    id: 2,
+    planname: 'styled components 조사 및 공부',
+  },
+  {
+    id: 3,
+    planname: '헬스장 출석 및 3대 측정하기',
+  },
+]
---
+++ app/renderer/src/screens/components/type.ts
@@ -0,0 +1,4 @@
+export interface PlanTagType {
+  id: number
+  planname: string
+}
