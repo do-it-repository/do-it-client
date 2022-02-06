58ba8eecfc86dc2182a1a1f6a25c6582fc6b9370
seongwoo <he1236@ajou.ac.kr>
2022-02-06 15:18:44 +0900

    AddTodoButton component clickoutside 및 이벤트 기본구현 완료

--- app/renderer/src/domain/todolist/components/ShowTodoList/AddTodoButton.tsx
+++ app/renderer/src/domain/todolist/components/ShowTodoList/AddTodoButton.tsx
@@ -1,16 +1,31 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { IconButton } from '@mui/material'
 import { Box, TextField } from '@mui/material'
 import AddIcon from '@mui/icons-material/Add'
+import { styled } from '@mui/material'
 
-export default function AddTodoButton(): JSX.Element {
-  const [AddModeOn, setAddModeOn] = useState<boolean>(false)
-  const onClick = (e: React.MouseEvent<HTMLInputElement>) => {
-    setAddModeOn(!AddModeOn)
-  }
-  if (AddModeOn === true) {
-    return <TextField />
-  }
+interface InputModeParam {
+  onEnter: (e: React.KeyboardEvent<HTMLInputElement>) => void
+  innerRef: any
+}
+
+interface ButtonModeParam {
+  onClick: (e: React.MouseEvent<HTMLInputElement>) => void
+}
+
+const InputMode = ({ onEnter, innerRef }: InputModeParam) => {
+  return (
+    <InputModeTextField
+      onKeyDown={onEnter}
+      ref={innerRef}
+      variant="standard"
+      placeholder="새로운 플랜을 입력하고 엔터"
+      autoFocus
+    />
+  )
+}
+
+const ButtonMode = ({ onClick }: ButtonModeParam) => {
   return (
     <Box onClick={onClick}>
       <IconButton>
@@ -19,3 +34,48 @@ export default function AddTodoButton(): JSX.Element {
     </Box>
   )
 }
+
+function clickOutside(
+  setInputMode: React.Dispatch<React.SetStateAction<boolean>>,
+  ref: any,
+) {
+  useEffect(() => {
+    function handleClickOutside(event: any) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        setInputMode((prev) => !prev)
+      }
+    }
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [ref])
+}
+
+export default function AddTodoButton(): JSX.Element {
+  const [inputMode, setInputMode] = useState<boolean>(false)
+  const wrapperRef = useRef(null)
+  const onClick = (e: React.MouseEvent<HTMLInputElement>) => {
+    setInputMode(!inputMode)
+  }
+  const onEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      setInputMode(!inputMode)
+    }
+  }
+
+  clickOutside(setInputMode, wrapperRef)
+
+  if (inputMode === true) {
+    return <InputMode onEnter={onEnter} innerRef={wrapperRef} />
+  }
+  return <ButtonMode onClick={onClick} />
+}
+
+// https://github.com/do-it-repository/do-it-client/blob/feature/TodoDetail/app/renderer/src/domain/todolist/components/TodoListArea/AddTodoArea.tsx
+
+const InputModeTextField = styled(TextField)({
+  minWidth: 270,
+  marginTop: 1,
+  marginLeft: 22,
+})
