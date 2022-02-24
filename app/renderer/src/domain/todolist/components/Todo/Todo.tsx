4fff5f0675f164a79f387919649a969a07354450
horizD <he1236@ajou.ac.kr>
2022-02-24 17:12:04 +0900

    Main Plan & Category & Hour Modify Event 간이 설정

--- app/renderer/src/domain/todolist/components/Todo/Todo.tsx
+++ app/renderer/src/domain/todolist/components/Todo/Todo.tsx
@@ -1,8 +1,9 @@
 import React, { useState, useRef, useEffect } from 'react'
 import { Box } from '@mui/material'
-import { List, ListItemText, Divider } from '@mui/material'
+import { List, ListItemText, Divider, TextField } from '@mui/material'
 import Badge, { BadgeProps } from '@mui/material/Badge'
 import NativeSelect from '@mui/material/NativeSelect'
+import Select from '@mui/material/Select'
 
 import { styled } from '@mui/material'
 import RecoilState from 'recoil'
@@ -11,22 +12,7 @@ import BasicSpeedDial from '../../../../common/components/BasicSpeedDial'
 import { TodoPropType, PrimaryPropType, SubTodoPropType } from '../../types'
 import { fontWeight } from '@mui/system'
 
-function clickOutside(
-  setIsActive: React.Dispatch<React.SetStateAction<boolean>>,
-  ref: any,
-) {
-  useEffect(() => {
-    function handleClickOutside(event: any) {
-      if (ref.current && !ref.current.contains(event.target)) {
-        setIsActive(false)
-      }
-    }
-    document.addEventListener('mousedown', handleClickOutside)
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside)
-    }
-  }, [ref])
-}
+
 
 const SubTodo = ({ subTodoList }: SubTodoPropType): JSX.Element => {
   return (
@@ -44,9 +30,27 @@ const SubTodo = ({ subTodoList }: SubTodoPropType): JSX.Element => {
 }
 
 export default function Todo({ todo }: TodoPropType): JSX.Element {
-  const wrapperRef = useRef(null)
+  
+  const Category = () => {
+    const wrapperRef = useRef(null)
 
-  const Category = ({innerRef}: any) => {
+    function clickOutside(
+      setIsActive: React.Dispatch<React.SetStateAction<boolean>>,
+      ref: any,
+    ) {
+      useEffect(() => {
+        function handleClickOutside(event: any) {
+          if (ref.current && !ref.current.contains(event.target)) {
+            setIsActive(false)
+          }
+        }
+        document.addEventListener('mousedown', handleClickOutside)
+        return () => {
+          document.removeEventListener('mousedown', handleClickOutside)
+        }
+      }, [ref])
+    }
+
     const [isActive,setIsActive] = useState<boolean>(false)
     
     const onClickCat = (e: React.MouseEvent<HTMLInputElement>) => (
@@ -57,17 +61,17 @@ export default function Todo({ todo }: TodoPropType): JSX.Element {
 
     if (isActive === true) {
       return (
-        <InlineBox ref={innerRef}>
+        <InlineBox ref={wrapperRef}>
             <NativeSelect
               defaultValue={`${category.name}`}
               
-              
-               >
+              >
                  <option value={`${category.name}`}>{`${category.name}${category.emoji}`}</option>
             </NativeSelect>
         </InlineBox>
       )
     }
+    
     return (
       <InlineBox onClick={onClickCat}>
         <InlineBox sx={{ fontWeight: 'bold' }}>
@@ -78,19 +82,80 @@ export default function Todo({ todo }: TodoPropType): JSX.Element {
     )
   }
 
+/*   const Hour = () => {
+    const [isActive,setIsActive] = useState<boolean>(false)
+
+    const onClickHour = (e: React.MouseEvent<HTMLInputElement>) => (
+      setIsActive(!isActive)
+    )
+    if (isActive === true) 
+    return (
+        <TextField size='small'/>
+            )
+    return (
+    <StyledBadge onClick={onClickHour} badgeContent={`${durationHour}시간`} color="primary">
+      
+    </StyledBadge>
+    )
+  }
+ */
   const { id, plan, category, durationHour, detailedText, subTodoList } = todo
 
+  const MainPlan = ():JSX.Element => {
+
+    const wrapperRef = useRef(null)
+
+    function clickOutside(
+      setIsActive: React.Dispatch<React.SetStateAction<boolean>>,
+      ref: any,
+    ) {
+      useEffect(() => {
+        function handleClickOutside(event: any) {
+          if (ref.current && !ref.current.contains(event.target)) {
+            setIsActive(false)
+          }
+        }
+        document.addEventListener('mousedown', handleClickOutside)
+        return () => {
+          document.removeEventListener('mousedown', handleClickOutside)
+        }
+      }, [ref])
+    }
+
+
+    const [isActive,setIsActive] = useState<boolean>(false)
+
+    clickOutside(setIsActive,wrapperRef)
+
+    const onDbClickPlan = (e:React.MouseEvent<HTMLInputElement>) => {
+      setIsActive(!isActive)
+    }
+    
+    if (isActive === true) {
+      return(
+      <TextField ref={wrapperRef} size='small' autoFocus={true} value={plan} placeholder='Type your main plan'/>
+      )
+      }
+
+    return (
+      <InlineBox onDoubleClick={onDbClickPlan} >{plan}</InlineBox>
+    )
+  }
+  
   const Primary = ({
     durationHour,
     category,
     plan,
   }: PrimaryPropType): JSX.Element => {
+
     return (
       <Box>
         <StyledBadge badgeContent={`${durationHour}시간`} color="primary">
-          <Category innerRef={wrapperRef}/>
-        </StyledBadge>
-        <Box sx={{ marginTop: 1, fontWeight: 'bold' }}>{plan}</Box>
+          <Category/>
+          </StyledBadge>
+        <Box  sx={{ marginTop: 1, fontWeight: 'bold' }}>
+          <MainPlan />
+        </Box>
       </Box>
     )
   }
